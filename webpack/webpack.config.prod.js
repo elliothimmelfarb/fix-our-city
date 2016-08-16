f8daae18e6527c91766938199abe85c549058acf
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-15 21:48:32 -0700

    fixed

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -118,54 +118,53 @@ module.exports = [
       new InlineEnviromentVariablesPlugin({ NODE_ENV: 'production' }),
       new webpack.DefinePlugin({ "global.GENTLY": false }),
     ],
-  ],
-  postcss: postCSSConfig
-}, {
-  // The configuration for the server-side rendering
-  name: 'server-side rendering',
-  context: path.join(__dirname, '..', 'app'),
-  entry: {
-    server: './server'
-  },
-  node: {
-    __dirname: true,
-  },
-  target: 'node',
-  output: {
-    // The output directory as absolute path
-    path: assetsPath,
-    // The filename of the entry chunk as relative path inside the output.path directory
-    filename: 'server.js',
-    // The output path from the view of the Javascript
-    publicPath: publicPath,
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    loaders: commonLoaders
-  },
-  resolve: {
-    root: [path.join(__dirname, '..', 'app')],
-    extensions: ['', '.js', '.jsx', '.css'],
-  },
-  plugins: [
-    // Order the modules and chunks by occurrence.
-    // This saves space, because often referenced modules
-    // and chunks get smaller ids.
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new ExtractTextPlugin('styles/main.css', { allChunks: true }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      __DEVCLIENT__: false,
-      __DEVSERVER__: false
-    }),
-    new webpack.IgnorePlugin(/vertx/),
-    new InlineEnviromentVariablesPlugin({ NODE_ENV: 'production' }),
-    new webpack.DefinePlugin({ "global.GENTLY": false }),
-  ],
-  postcss: postCSSConfig
-}
+    postcss: postCSSConfig
+  }, {
+    // The configuration for the server-side rendering
+    name: 'server-side rendering',
+    context: path.join(__dirname, '..', 'app'),
+    entry: {
+      server: './server'
+    },
+    node: {
+      __dirname: true,
+    },
+    target: 'node',
+    output: {
+      // The output directory as absolute path
+      path: assetsPath,
+      // The filename of the entry chunk as relative path inside the output.path directory
+      filename: 'server.js',
+      // The output path from the view of the Javascript
+      publicPath: publicPath,
+      libraryTarget: 'commonjs2'
+    },
+    module: {
+      loaders: commonLoaders
+    },
+    resolve: {
+      root: [path.join(__dirname, '..', 'app')],
+      extensions: ['', '.js', '.jsx', '.css'],
+    },
+    plugins: [
+      // Order the modules and chunks by occurrence.
+      // This saves space, because often referenced modules
+      // and chunks get smaller ids.
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new ExtractTextPlugin('styles/main.css', { allChunks: true }),
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        __DEVCLIENT__: false,
+        __DEVSERVER__: false
+      }),
+      new webpack.IgnorePlugin(/vertx/),
+      new InlineEnviromentVariablesPlugin({ NODE_ENV: 'production' }),
+      new webpack.DefinePlugin({ "global.GENTLY": false }),
+    ],
+    postcss: postCSSConfig
+  }
 ];

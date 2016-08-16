69e1e2c85fbf3a9ac2c1964376e4fe845ec595eb
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-15 22:01:31 -0700

    commitable

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -21,7 +21,7 @@ const cx = classNames.bind(styles);
    backgroundImage: "url('https://image.pbs.org/poster_images/assets/01mpmdkk0npu1svmvp6s_1.png.resize.710x399.png'), url('http://www.ibox-security.co.uk/wp-content/uploads/2015/07/ss-2-bg.jpg')",
    backgroundRepeat: 'repeat-x, repeat-y',
    backgroundPosition: 'bottom',
-   height: '100vh',
+   height: '100%',
    width: '100vw',
    backgroundColor: 'skyblue',
    padding: '5%',

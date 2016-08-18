20e60eff3d8e20f540475a91c65dfb4b18df3e85
Thomas Wolfe <tewolfe2@gmail.com>
2016-08-18 13:32:24 -0700

    card view issues fixed

--- app/components/viewIssues/CardView.jsx
+++ app/components/viewIssues/CardView.jsx
@@ -1,18 +1,20 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardHeader, CardText, CardMedia } from 'material-ui/Card';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
 import Badge from 'material-ui/Badge';
-import { Col } from 'react-flexbox-grid';
+import { Card, CardHeader, CardText, CardMedia, CardActions, CardTitle } from 'material-ui/Card';
+import { Row, Col } from 'react-flexbox-grid';
+import moment from 'moment';
+import Toggle from 'material-ui/Toggle';
 import { upvoteIssue, downvoteIssue, selectIssue } from '../../actions/issueActions';
 
 const styles = {
   image: {
     // maxHeight: 500,
     // maxWidth: 500,
-    width: '80%',
-    height: '80%',
+    width: '90%',
+    height: '90%',
     margin: '0 auto',
     textAlign: 'center',
   },
@@ -24,7 +26,6 @@ const styles = {
     display: 'inline',
   },
 };
-
 class CardView extends React.Component {
   constructor(props) {
     super(props);
@@ -40,6 +41,22 @@ class CardView extends React.Component {
     }
   }
   render() {
+    const tagStyle = {
+      backgroundColor: '#eee',
+      borderRadius: '20px',
+      border: '1px solid #eee',
+      wordWrap: 'break-word',
+    };
+    const toggleStyles = {
+      marginBottom: 16,
+    };
+    const descriptionStyle = {
+      wordWrap: 'break-word',
+    };
+    const subStyle = {
+      whiteSpace: 'pre-line',
+    };
+
     const props = this.props;
     const upvoteStatus = props.upvoted.includes(props._id);
     const downvoteStatus = props.downvoted.includes(props._id);
@@ -56,32 +73,49 @@ class CardView extends React.Component {
           actAsExpander
         >
           <CardText style={styles.voting}>
-            <IconButton
-              disabled={upvoteStatus}
-              onClick={() => props.upvoteIssue(props._id, props.mapCenter, props.mapCorner)}
-            >
-              <FontIcon className="material-icons">thumb_up</FontIcon>
-            </IconButton>
-            <Badge
-              badgeContent={props.votes}
-              secondary
-              badgeStyle={{ top: 5, right: 5 }}
-            />
-            <IconButton
-              disabled={downvoteStatus}
-              onClick={() => props.downvoteIssue(props._id, props.mapCenter, props.mapCorner)}
-            >
+          <IconButton
+            disabled={upvoteStatus}
+            onClick={() => props.upvoteIssue(props._id, props.mapCenter, props.mapCorner)}
+          >
+            <FontIcon className="material-icons">thumb_up</FontIcon>
+          </IconButton>
+          <Badge
+            badgeContent={props.votes}
+            secondary
+            badgeStyle={{ top: 5, right: 5 }}
+          />
+          <IconButton
+            disabled={downvoteStatus}
+            onClick={() => props.downvoteIssue(props._id, props.mapCenter, props.mapCorner)}
+          >
+            <FontIcon className="material-icons">thumb_down</FontIcon>
+          </IconButton>
+        </CardText>
+      </CardHeader>
 
-              <FontIcon className="material-icons">thumb_down</FontIcon>
-            </IconButton>
-          </CardText>
-        </CardHeader>
-        <CardMedia
-          expandable
-          mediaStyle={styles.image}
-        >
-          <img src={`${props.imgUrl}`} alt="Issue" style={styles.image} />
-        </CardMedia>
+      <CardMedia
+        expandable
+        mediaStyle={styles.image}
+      >
+        <img src={`${props.imgUrl}`} alt="Issue" style={styles.image} />
+      </CardMedia>
+      <CardText expandable>
+        <Row >
+          <Col xs={7} md={10} lg={10} style={descriptionStyle}>
+            {props.description}
+          </Col>
+          <Col xs={5} md={2} lg={2}>
+            <Toggle
+              style={toggleStyles}
+              labelPosition="right"
+              label="Fixed?"
+            />
+          </Col>
+        </Row>
+      </CardText>
+      <CardActions>
+        <button style={tagStyle}>test</button>
+      </CardActions>
       </Card>
     );
   }

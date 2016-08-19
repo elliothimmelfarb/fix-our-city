4340bca8fefdd5d6843cdde43624f67e1428ea76
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-19 14:33:24 -0700

    background monster vanquished

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,7 +5,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Navbar';
 import styles from '../css/main.css';
 
-
 const cx = classNames.bind(styles);
 
 
@@ -19,29 +18,33 @@ const cx = classNames.bind(styles);
  * https://github.com/rackt/react-router/blob/latest/docs/Introduction.md
  <Navigation />
  */
-
-// const bgImage = {
-//   backgroundImage: "url('https://image.pbs.org/poster_images/assets/01mpmdkk0npu1svmvp6s_1.png.resize.710x399.png')",
-//   backgroundRepeat: 'repeat-x',
-//   //  backgroundImage: "url('https://image.pbs.org/poster_images/assets/01mpmdkk0npu1svmvp6s_1.png.resize.710x399.png'), url('http://www.ibox-security.co.uk/wp-content/uploads/2015/07/ss-2-bg.jpg')",
-//   //  backgroundRepeat: 'repeat-x, repeat-y',
-//   backgroundPosition: 'bottom',
-//   minHeight: '100vh',
-//   width: '100vw',
-//   backgroundColor: '#00bcd4',
-//   paddingTop: '7%',
-// };
+ const bgImage = {
+   backgroundImage: "url('https://image.pbs.org/poster_images/assets/01mpmdkk0npu1svmvp6s_1.png.resize.710x399.png')",
+   backgroundRepeat: 'repeat-x',
+  //  backgroundImage: "url('https://image.pbs.org/poster_images/assets/01mpmdkk0npu1svmvp6s_1.png.resize.710x399.png'), url('http://www.ibox-security.co.uk/wp-content/uploads/2015/07/ss-2-bg.jpg')",
+  //  backgroundRepeat: 'repeat-x, repeat-y',
+   backgroundPosition: 'bottom',
+   minHeight: '100vh',
+   width: '100vw',
+   backgroundColor: '#00bcd4',
+   paddingTop: '7%',
+ };
+ const navbarStyle = {
+   position: 'fixed',
+   top: '0px',
+   left: '0px',
+ };
 const App = ({ children }) => (
-  <div className={styles.bgImage}>
-    <div className={cx('app')}>
-      <MuiThemeProvider>
-        <Grid>
-          <Navbar />
-          {children}
-        </Grid>
-      </MuiThemeProvider>
-    </div>
+
+  <div className={cx('app')} style={bgImage}>
+    <MuiThemeProvider>
+      <Grid>
+        <Navbar />
+        {children}
+      </Grid>
+    </MuiThemeProvider>
   </div>
+
 );
 
 
